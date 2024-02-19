8e0dba91de413745892f3179913baaa9751dc336
b0rnsl1ppy <rnb.wrk@gmail.com>
2024-02-19 14:17:58 +0530

    changes for UserList

--- src/UserList.js
+++ src/UserList.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function UserList() {
-  const [users, setUsers] = useState([]); // State to store fetched users
+  // State to store fetched users
+  const [users, setUsers] = useState([]);
 
+  // Effect to fetch users from GitHub API when the component mounts
   useEffect(() => {
-    fetch('https://api.github.com/users') // Fetch users from GitHub API
+    fetch('https://api.github.com/users')
       .then(response => response.json()) // Parse response as JSON
       .then(data => setUsers(data)) // Update state with fetched users
       .catch(error => console.error('Error fetching users:', error)); // Log any errors
@@ -15,14 +17,28 @@ function UserList() {
     <div>
       <h1>User List</h1>
       <ul>
-        {/* Map through users and render list items with links to UserDetails component */}
+        {/* 
+          Map through users and render list items with links to UserDetails component.
+          Each list item corresponds to a GitHub user.
+        */}
         {users.map(user => (
-          <ol key={user.id}>
+          <li key={user.id}>
+            {/* 
+              Create a link to the UserDetails component for each user.
+              Clicking on the link will navigate to the UserDetails page for the respective user.
+            */}
             <Link to={`/user/${user.login}`}>
+              {/* Render the user's avatar image */}
               <img src={user.avatar_url} alt="Avatar" />
-              {user.name ? `${user.name} (${user.login})` : user.login}
+              <div>
+                {/* No object data (K/V) in API for User's first & last name so N/A will be displayed */}
+                <p>First Name: {user.name ? user.name.split(' ')[0] : 'N/A'}</p>
+                <p>Last Name: {user.name ? user.name.split(' ')[1] : 'N/A'}</p>
+                {/* Render the user's login username */}
+                <p>Username: {user.login}</p>
+              </div>
             </Link>
-          </ol>
+          </li>
         ))}
       </ul>
     </div>
